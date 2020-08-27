b06f8bfe03cce79493f6a0921d22729e111ff2d6
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-27 22:30:47 +0200

    Add ability to view articles

---
+++ DAN_LVII_Natasa_Jevtic/Zadatak_1_Client/UserMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Zadatak_1_Client.ServiceReferenceArticle;
+
+namespace Zadatak_1_Client
+{
+    class UserMenu
+    {
+        public static void DisplayMenu()
+        {
+            string option = "";
+            do
+            {
+                Console.WriteLine("1. Add article");
+                Console.WriteLine("2. View articles");
+                Console.WriteLine("3. Purchase articles");
+                Console.WriteLine("4. Update article");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Choose option:");
+                option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":                        
+                        break;
+                    case "2":
+                        int counterOfItem = 0;
+                        Console.WriteLine("\nArticles:");
+                        using (Service1Client service = new Service1Client())
+                        {
+                            foreach (Article item in service.ViewArticles())
+                            {
+                                Console.WriteLine(++counterOfItem +". Name: " + item.Name + ", Quantity: " + item.Quantity + ", Price: " + item.Price);
+                            }
+                        }
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Try again:");
+                        break;
+                }
+            } while (option != "5");
+        }
+    }
+}
--- DAN_LVII_Natasa_Jevtic/Zadatak_1_WCF/Service1.cs
+++ DAN_LVII_Natasa_Jevtic/Zadatak_1_WCF/Service1.cs
@@ -10,32 +10,39 @@ namespace Zadatak_1_WCF
 {    
     public class Service1 : IService1
     {
-        string articleFolder = AppDomain.CurrentDomain.BaseDirectory + @"\..\..\Files";
-        string locationFile = AppDomain.CurrentDomain.BaseDirectory + @"\..\..\Files\Articles.txt";
+        readonly string articleFolder = AppDomain.CurrentDomain.BaseDirectory + @"\..\..\Files";
+        readonly string locationFile = AppDomain.CurrentDomain.BaseDirectory + @"\..\..\Files\Articles.txt";
 
         public List<Article> ViewArticles()
         {
+            List<Article> articles = new List<Article>();
             if (File.Exists(locationFile))
             {
-                List<Article> articles = new List<Article>();
-                string[] lines = File.ReadAllLines(locationFile);
-                List<string> list = new List<string>();
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    list = lines[i].Split(',').ToList();
-                    Article article = new Article()
+                try
+                {                    
+                    string[] lines = File.ReadAllLines(locationFile);
+                    List<string> list = new List<string>();
+                    for (int i = 0; i < lines.Length; i++)
                     {
-                        Name = list[0],
-                        Quantity = Int32.Parse(list[1]),
-                        Price = Double.Parse(list[2])
-                    };                    
-                    articles.Add(article);
+                        list = lines[i].Split(',').ToList();
+                        Article article = new Article()
+                        {
+                            Name = list[0],
+                            Quantity = Int32.Parse(list[1]),
+                            Price = Double.Parse(list[2])
+                        };
+                        articles.Add(article);
+                    }
+                    return articles;
                 }
-                return articles;
+                catch (Exception)
+                {
+                    return articles;                    
+                }                
             }
             else
             {
-                return null;
+                return articles;
             }
         }
     }
